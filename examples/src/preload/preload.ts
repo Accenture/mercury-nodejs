199877872c8375df2af160ca86878373ef6bd294
Eric Law <39393654+acn-ericlaw@users.noreply.github.com>
2024-12-16 12:54:28 -0800

    update example

--- examples/src/preload/preload.ts
+++ examples/src/preload/preload.ts
@@ -9,7 +9,7 @@ import { parse as parseYaml } from 'yaml';
 import { fileURLToPath } from "url";
 import { Logger, Platform, ConfigReader, FunctionRegistry } from 'mercury';
 // import the user services
-// Generated: 2024-12-15 11:20:50.958
+// Generated: 2024-12-16 12:52:38.074
 import { DemoAuth } from '../services/demo-auth.js';
 import { DemoHealthCheck } from '../services/health-check.js';
 import { HelloWorldService } from '../services/hello-world-service.js';
